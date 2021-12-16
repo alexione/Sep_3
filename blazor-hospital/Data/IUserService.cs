1f783bf995c902a58323ef2752e1ac7332ef14ce
Ionut <ionutrotaru077@gmail.com>
2021-12-16 13:55:14 +0100

    Initial Commit

---
+++ blazor-hospital/Data/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using blazor_hospital.Models;
+
+namespace blazor_hospital.Data
+{
+    public interface IUserService
+    {
+        User validateUser(string userName, string password);
+    }
+}
---
+++ blazor-hospital/Models/RoomModel.cs
@@ -0,0 +1,11 @@
+﻿namespace blazor_hospital.Models
+{
+    public class RoomModel
+    {
+        public long room_number { get; set; }
+        public string room_type { get; set; }
+        public string status { get; set; }
+
+
+    }
+}
